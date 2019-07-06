73fd695ebef08202e7ee4aadb6c092ced2f36c3f
Soren <50107716+soren000@users.noreply.github.com>
2019-07-06 11:48:06 -0700

    Update sendResponse.js

--- src/discord/sendResponse.js
+++ src/discord/sendResponse.js
@@ -2,7 +2,7 @@ const embedGen = require('./embedGen');
 
 const commandCheck = async (message, param) => {
     // console.log(param);
-    message.channel.send(embedGen({title: "poop" }));
+    message.channel.send(embedGen({title: "asd" }));
     return;
 }
 
